2a7ddb9c254d5879923f9eec7d7aedc3ce127a7e
licjosechavez <t10_chavez@hotmail.com>
2022-09-20 14:19:03 -0300

    First commit

---
+++ src/main/java/com/global/TP_Final_Integrador/TpFinalIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.global.TP_Final_Integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/TP_Final_Integrador/TpFinalIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.TP_Final_Integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
