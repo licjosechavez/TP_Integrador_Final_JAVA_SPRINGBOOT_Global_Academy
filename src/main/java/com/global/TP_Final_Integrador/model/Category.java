1cc80fca63068163ee94be58f7ba924592f74c00
licjosechavez <t10_chavez@hotmail.com>
2022-09-20 20:50:50 -0300

    Upload Category model

--- src/main/java/com/global/TP_Final_Integrador/model/Category.java
+++ src/main/java/com/global/TP_Final_Integrador/model/Category.java
@@ -1,2 +1,47 @@
-package com.global.TP_Final_Integrador.model;public class Category {
+package com.global.TP_Final_Integrador.model;
+
+public class Category {
+
+    public String name;
+    public String subCategory;
+
+    //constructs
+
+    public Category() {
+    }
+
+    public Category(String name, String subCategory) {
+        this.name = name;
+        this.subCategory = subCategory;
+    }
+
+    //getters y setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    //toString
+
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", subCategory='" + subCategory + '\'' +
+                '}';
+    }
 }
